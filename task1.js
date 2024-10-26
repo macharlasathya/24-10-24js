41d3b017018016225f1ab0db80b1ee36d5d7952e
macharlasathya <sathishmacharla5995@gmail.com>
2024-10-26 20:57:05 +0530

    adding the files

---
+++ task1.js
@@ -0,0 +1,57 @@
+switch("Monday")
+{
+    case "Monday":
+        console.log("Today is Monday")
+        break
+    case "Tuseday":
+        console.log("Today is Tuseday")
+        break
+    case "Wednesday":
+        console.log("Today is wednesday")
+        break
+    case "Thursday":
+        console.log("Today is Thursday")
+        break
+    case "Friday":                                           ///question 1//
+        console.log("Today is Friday")
+        break
+    case "saturday":
+        console.log("Today is saturday")
+        break
+}
+
+var Input=prompt("enter the day:")
+switch(Input)
+{
+    case "Monday":
+        console.log("Today is Monday")
+        break
+    case "Tuseday":                                            //question 1//
+        console.log("Today is Tuseday")
+        break
+    case "Wednesday":
+        console.log("Today is wednesday")
+        break
+    case "Thursday":
+        console.log("Today is Thursday")
+        break
+    case "Friday":
+        console.log("Today is Friday")
+        break
+    case "saturday":
+        console.log("Today is saturday")
+        break
+}
+
+var a=[1,2,3,4,5]
+for( numbers of a)
+{                                                   //question 2//
+    console.log(numbers)
+}
+
+
+var Hero="Prabha"                                //question 3//
+for(char of Hero)
+{
+    console.log(char)
+}
